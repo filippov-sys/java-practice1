99058a4ab93d85e14faf7e4608a7ba0a2031ce66
filippov-sys <metrkelo@gmail.com>
2026-09-08 15:05:25 +0300

    zadanie3

---
+++ zadaniye3.java
@@ -0,0 +1,9 @@
+public class zadaniye3 {
+    public static void main(String[] args) {
+        for (int pervoe = 1; pervoe <= 5; pervoe++) {
+            for (int vtoroe = 1; vtoroe <= 5; vtoroe++)
+                System.out.println(pervoe + " * " + vtoroe + " = " + pervoe * vtoroe);
+            }
+        }
+    }
+

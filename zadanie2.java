504245ec34563c0cfb12f567e5c3e30aab0af93a
filippov-sys <metrkelo@gmail.com>
2026-09-08 15:04:44 +0300

    zadanie2

---
+++ zadanie2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class zadanie2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 5, 6, 3, 6, 9, 3, 44, 6, 10, 19};
+        System.out.println(Arrays.toString(numbers));
+        int sumchet = 0;
+        int nechetcount = 0;
+
+        for (int i= 0; i < numbers.length; i++) {
+            System.out.println(numbers[i] + " ");
+            if (numbers[i] % 2 == 0) {
+                sumchet += numbers[i];
+            } else {
+                nechetcount++;
+            }
+        }
+
+        System.out.println("сумма всех четных: " + sumchet);
+        System.out.println("количество всех нечетных: " + nechetcount);
+    }
+}
